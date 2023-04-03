634d3fd373a93511dff160e7411aa4c2883b82da
Agnieszka <annies@o2.pl>
2023-04-03 09:31:46 +0200

    Added Package and CreatePackage Page

---
+++ src/pages/CreatePackage.jsx
@@ -0,0 +1,159 @@
+import React, {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import {useDispatch} from "react-redux";
+import {createPackage} from "../features/package/packageSlice";
+
+
+export const CreatePackage = () => {
+
+    // const [packageImg, setPackageImg] = useState("");
+    const [type, setType] = useState("");
+    const [material, setMaterial] = useState("");
+    const [surface, setSurface] = useState("");
+
+    const [packageData, setPackageData] = useState(
+        {
+            code: '',
+            diameter: '',
+            width: '',
+        });
+
+    const { code, diameter, width } = packageData;
+
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+
+    // const handlePackageImageUpload = (e) => {
+    //     const file = e.target.files[0];
+    //
+    //     TransformFile(file);
+    // };
+    //
+    // const TransformFile = (file) => {
+    //     const reader = new FileReader();
+    //
+    //     if(file) {
+    //         reader.readAsDataURL(file)
+    //         reader.onloadend = () => {
+    //             setPackageImg(reader.result);
+    //         };
+    //     } else {
+    //         setPackageImg("")
+    //     }
+    // }
+
+    const onChange = (e) => {
+        setPackageData((prevState) => ({
+            ...prevState,
+            [e.target.name] : e.target.value,
+        }))
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+        dispatch(createPackage({
+            code,
+            diameter,
+            width,
+            type,
+            material,
+            surface,
+            // image: packageImg,
+        }))
+        navigate("/packages")
+    }
+
+    return (
+        <>
+            <div className="form-wrapper">
+                <div className="heading">
+                    <h3>Create Package</h3>
+                </div>
+                <section className="form">
+                    <form onSubmit={onSubmit}>
+                        {/*<div className="form-group">*/}
+                        {/*    <input type="file" accept="image/" onChange={handlePackageImageUpload}/>*/}
+                        {/*</div>*/}
+                        {/*<div className="image_preview">*/}
+                        {/*    {packageImg ? (*/}
+                        {/*        <>*/}
+                        {/*            <img src={packageImg} alt="packaging image"/>*/}
+                        {/*        </>*/}
+                        {/*    ) : (*/}
+                        {/*        <p>Place for image preview</p>*/}
+                        {/*    )}*/}
+                        {/*</div>*/}
+                        <div className="form-group">
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="code"
+                                name="code"
+                                value={code}
+                                placeholder="Packaging code"
+                                onChange={onChange}/>
+                        </div>
+                        <div className='form-group'>
+                            <select onChange={(e) => setType(e.target.value)} required>
+                                <option value="">Select packaging type</option>
+                                <option value="jar">jar</option>
+                                <option value="bottle">bottle</option>
+                                <option value="pot">pot</option>
+                                <option value="tube">tube</option>
+                                <option value="pen">pen</option>
+                                <option value="liquidDispensing">liquid dispensing</option>
+                            </select>
+                        </div>
+                        <div className='form-group'>
+                            <input
+                                type="text"
+                                className="form-control"
+                                name="diameter"
+                                value={diameter}
+                                placeholder="Diameter [mm]"
+                                onChange={onChange}/>
+                        </div>
+                        <div className='form-group'>
+                            <input
+                                type="text"
+                                className="form-control"
+                                name="width"
+                                value={width}
+                                placeholder="Width [mm]"
+                                onChange={onChange}/>
+                        </div>
+                        <div className='form-group'>
+                            <select onChange={(e) => setMaterial(e.target.value)} required>
+                                <option value="">Select packaging material</option>
+                                <option value="PP">PP</option>
+                                <option value="PP_airless">PP airless</option>
+                                <option value="pet">PET</option>
+                                <option value="sans">SANS</option>
+                                <option value="acrylic">acrylic</option>
+                                <option value="acrylic_airless">acrylic airless</option>
+                                <option value="glass">glass</option>
+                            </select>
+                        </div>
+                        <div className='form-group'>
+                            <select onChange={(e) => setSurface(e.target.value)} required>
+                                <option value="">Select packaging surface</option>
+                                <option value="smooth">smooth</option>
+                                <option value="rough">rough</option>
+                                <option value="grooved">grooved</option>
+                            </select>
+                        </div>
+                        <div className="form-group">
+                            <button className="btn btn-block" type="submit">
+
+                                Add Packaging
+                            </button>
+                        </div>
+                    </form>
+                </section>
+            </div>
+        </>
+    )
+}
---
+++ src/pages/Package.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {Outlet, useNavigate} from "react-router-dom";
+import {Sidebar} from "../components/Sidebar/Sidebar";
+import {FaPlus} from "react-icons/fa";
+
+export const Package = () => {
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <div className="container-wrapper">
+                <Sidebar/>
+                <section className="main-content">
+                    <div className="heading">
+                        <h3>Package</h3>
+                        <div className="btn-create">
+                            <button className="btn btn-add" onClick={() => navigate('/packages/create-package')}>
+                                <FaPlus/>
+                                Create
+                            </button>
+                        </div>
+
+                        <Outlet />
+                    </div>
+                </section>
+            </div>
+        </>
+    )
+}
