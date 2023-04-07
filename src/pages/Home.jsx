0e57fea16955950698b9ca6e6605d795f95764c9
Agnieszka <annies@o2.pl>
2023-04-07 09:21:09 +0200

    Added home page ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,16 @@
 import React from "react";
-
+import image from "../images/bottles_packages_.jpg";
 export const Home = () => {
     return (
         <>
-
+            <div className="container">
+                <div className="home-container">
+                    <div className="home-text">
+                        <h2 >Product composer</h2>
+                    </div>
+                    <img src={image} alt="cosmetic set" className="img"/>
+                </div>
+            </div>
         </>
     )
 }
